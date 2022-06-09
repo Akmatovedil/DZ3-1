5098d18db0094cd2b0882424daf4314f68cb2e8e
Akmatovedil <akmatov.ed@gmail.com>
2022-06-09 23:40:58 +0300

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,10 @@
+list1 = [1, 2, 3 ,4]
+list2 = [8, 12, 45, 67, 89, 45]
+list3 = [78, 90, 65]
+
+list1_copy = list1.copy()
+list1 = list1+list1_copy
+list2_copy = list2.copy()
+list2 = list2+list2_copy
+list3_copy = list3.copy()
+list3 = list3+list3_copy
